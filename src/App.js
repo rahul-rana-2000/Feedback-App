2e49dbbf54b6e319e9da3e9a2cf26a8009afdd42
rahul-rana-2000 <rahulrana9633@gmail.com>
2024-11-13 12:33:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Header from './components/Header'  
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
+import AboutPage from './Pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
+import Card from './components/shared/Card'
+import { FeedbackProvider } from './Context/FeedbackContext'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  return (
+    <FeedbackProvider>
+<Router>
+<Header />
+<div className='container'>
+  <Routes>
+ <Route exact path='/'
+ element={
+    <>
+  <FeedbackForm />
+  <FeedbackStats />
+<FeedbackList />
+</>
+ }>
+  
+  </Route>   
+
+<Route path='/about' element={<AboutPage/>} />
+</Routes>
+
+<Card>
+  <NavLink to='/' activeClassName='active'>
+  Home
+  </NavLink>
+  <NavLink to='/about' activeClassName='active'>
+  About 
+  </NavLink>
+</Card>
+<AboutIconLink />
+</div>
+    </Router>
+    </FeedbackProvider>
+  )
+  }
+
+export default App
+
+
+
